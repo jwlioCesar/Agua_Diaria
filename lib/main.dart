2a3a374434214d5900724bd314a058c95a50f784
Júlio César <juliocfds20@gmail.com>
2023-05-18 17:34:19 -0300

    aprendendo sobre rows e columns

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,13 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Quantidade de agua diaria',
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text('Água diária'),
-        actions: [
-          IconButton(icon: const Icon(Icons.refresh),
-              onPressed: (){})
-        ],
-      ),
+      home: TelaInicial(),
     );
   }
 }
 
 class TelaInicial extends StatefulWidget {
   const TelaInicial({Key? key}) : super(key: key);
-
   @override
   State<TelaInicial> createState() => _TelaInicialState();
 }
@@ -42,7 +24,44 @@ class TelaInicial extends StatefulWidget {
 class _TelaInicialState extends State<TelaInicial> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue,
+        title: const Text('Água diária'),
+        actions: [
+          IconButton(icon: const Icon(Icons.refresh),
+              onPressed: (){})
+        ],
+      ),
+      body:  Column(
+
+        children: [
+          Text('Quantos litros de água devo beber por dia?',
+          style: TextStyle(
+            color: Colors.cyan,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+
+          ),
+            textAlign: TextAlign.justify
+            ,
+          ),
+          Form(
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "QUAL É O SEU PESO?",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50)
+                ),
+              ),
+
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
